543e39289039373f9936cc4c3b20621c2bfcaffd
Nam Phan <phannam1412@yahoo.com>
2019-09-27 10:18:43 +0700

    added core parser

---
+++ core.go
@@ -0,0 +1,666 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+func PrintJson(test interface{}) {
+	tmp, _ := json.MarshalIndent(test, "", "    ")
+	fmt.Printf("%s\n", tmp)
+}
+
+func JsonEncode(test interface{}) string {
+	tmp, _ := json.MarshalIndent(test, "", "    ")
+	return string(tmp)
+}
+
+func isset(theMap map[string]string, key string) bool {
+	_, ok := theMap[key]
+	return ok
+}
+
+
+
+
+
+var tries Expression
+var mapping map[string]string
+var Whitespace Expression
+var SomeWhitespaces Expression
+var AnyWhitespaces Expression
+var Comma Expression
+var Dollar Expression
+
+func init() {
+
+	Comma = Text(",")
+	Dollar = Text("$")
+	Whitespace = Text(" ")
+	SomeWhitespaces = Some(Whitespace)
+	AnyWhitespaces = Any(Whitespace)
+
+	mapping = map[string]string {
+		"day of birth": "birthday",
+		"date of birth": "birthday",
+		"birthday": "birthday",
+
+		"e-mail address": "email",
+		"e-mail": "email",
+		"email": "email",
+
+		"home address": "address",
+		"contact address": "address",
+		"address": "address",
+
+		"height": "height",
+		"weight": "weight",
+
+		"full name": "fullname",
+
+		"phone number": "phone",
+		"hand phone": "phone",
+		"contact phone": "phone",
+		"tel": "phone",
+		"phone": "phone",
+		"mobile": "phone",
+
+		"sex": "sex",
+		"marital": "marital",
+		"dob": "birthday",
+		"position": "position",
+		"job level": "job_level",
+		"work place": "work_place",
+		"job category": "job_category",
+		"salary expected": "salary",
+		"salary": "salary",
+
+
+		"years of experience": "experience",
+		"experience": "experience",
+
+		"interests": "interest",
+
+
+		"highest degree": "highest_degree",
+		"language proficiency": "language_proficiency",
+		"most recent job": "most_recent_job",
+		"most recent company": "most_recent_company",
+		"current job level": "current_job_level",
+		"first name": "first_name",
+		"last name": "last_name",
+		"gender": "gender",
+		"nationality": "nationality",
+		"website": "website",
+		"web site": "website",
+		"birthplace": "birthplace",
+		"place of birth": "birthplace",
+		"languages": "language",
+		"language": "language",
+		"yahoo": "yahoo",
+		"skype": "skype",
+	}
+
+	var tmp []Expression
+	for wordForDetect := range mapping {
+		tmp = append(tmp, CaseInsensitive(Tokenize(wordForDetect)))
+	}
+	tries = Or(tmp...)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func Tokenize(text string) []string {
+	var res []string
+	word := ""
+	chars := "+ _)(*&^%#@!~[];/.,\\{}|:\"<>?`-='\n\r\t$"
+	for a := 0; a < len(text); a++ {
+		char := text[a : a + 1]
+		if !strings.Contains(chars, char) {
+			word += char
+		} else {
+			if len(word) > 0 {
+				res = append(res, word)
+			}
+			res = append(res, char)
+			word = ""
+		}
+	}
+	if len(word) > 0 {
+		res = append(res, word)
+	}
+	return res
+}
+
+type Expression func(tokens []string, pos int) *Res
+
+type Res struct {
+	Pos int
+	Value string
+	Expr string
+	Children []Res
+	Data map[string]string
+}
+
+func And(expressions ...Expression) Expression {
+	return func(tokens []string, pos int) *Res {
+		if pos >= len(tokens) {
+			return nil
+		}
+		var children []Res
+		var value []string
+		for _, exp := range expressions {
+			res := exp(tokens, pos)
+			if res == nil {
+				return nil
+			}
+			children = append(children, *res)
+			pos = res.Pos
+			value = append(value, res.Value)
+		}
+		return &Res{
+			Pos: pos,
+			Expr: "and",
+			Children: children,
+			Value: strings.Join(value, ""),
+		}
+	}
+}
+
+// Match all posibilities of all positions in the token list.
+func FullSearch(expr Expression, limit int) Expression {
+	return func(tokens []string, pos int) *Res {
+		var children []Res
+		var value []string
+
+		// Travel through every possible position of token list to find as much matches as possible.
+		for a := 0; a < len(tokens) && pos < len(tokens); a++ {
+
+			// Is there any match at this position ?
+			res := expr(tokens, pos)
+
+			// No match ? Find at next position.
+			if res == nil {
+				pos++
+				continue
+			}
+			pos = res.Pos
+			children = append(children, *res)
+			value = append(value, res.Value)
+			if len(value) == limit && limit != -1 {
+				break
+			}
+		}
+
+		if value == nil {
+			return nil
+		}
+
+		return &Res{
+			Pos: pos,
+			Expr: "full_search",
+			Children: children,
+			Value: strings.Join(value, ""),
+		}
+	}
+}
+
+func LookupForOne(expr Expression) Expression {
+	return func(tokens []string, pos int) *Res {
+		for a := 0; a < len(tokens) && pos < len(tokens); a++ {
+			res := expr(tokens, pos)
+			if res != nil {
+				return &Res{
+					Pos: res.Pos,
+					Expr: "lookup_for_one",
+					Value: res.Value,
+				}
+			}
+			pos++
+		}
+		return nil
+	}
+}
+
+func AtLeast(expr Expression, count int) Expression {
+	return func(tokens []string, pos int) *Res {
+		var value []string
+		for a := 0; a < len(tokens) && pos < len(tokens); a++ {
+			res := expr(tokens, pos)
+			if res != nil {
+				return nil
+			}
+			value = append(value, res.Value)
+			if a >= count {
+				return &Res{
+					Pos: res.Pos,
+					Expr: "at_least",
+					Value: strings.Join(value, " "),
+				}
+			}
+			pos++
+		}
+		return nil
+	}
+}
+
+func Some(expr Expression) Expression {
+	return func(tokens []string, pos int) *Res {
+		var value []string
+		for a := 0; a < len(tokens) && pos < len(tokens); a++ {
+			res := expr(tokens, pos)
+			if res == nil {
+				if len(value) > 0 {
+					return &Res{
+						Pos: pos,
+						Expr: "at_least",
+						Value: strings.Join(value, " "),
+					}
+				}
+				return nil
+			}
+			value = append(value, res.Value)
+			pos++
+		}
+		if len(value) > 0 {
+			return &Res{
+				Pos: pos,
+				Expr: "at_least",
+				Value: strings.Join(value, " "),
+			}
+		}
+		return nil
+	}
+}
+
+// Match from 0 -> as much as possible
+// Match all posibilities starting at the current pos, limit by min.
+func Any(expr Expression) Expression {
+	return func(tokens []string, pos int) *Res {
+		var values []string
+		var children []Res
+		a := 0
+		for a = 0; a < len(tokens) && pos < len(tokens); a++ {
+			res := expr(tokens, pos)
+
+			// Cannot match anymore ?
+			if res == nil {
+				return &Res{
+					Pos: pos,
+					Expr: "any",
+					Children: children,
+					Value: strings.Join(values, " "),
+				}
+			}
+
+			values = append(values, res.Value)
+			children = append(children, *res)
+			pos = res.Pos
+		}
+
+		return &Res{
+			Pos: pos,
+			Expr: "greedy",
+			Children: children,
+			Value: strings.Join(values, " "),
+		}
+	}
+}
+
+// Match all posibilities starting at the current pos, limit by min.
+func Greedy(expr Expression, min int) Expression {
+	return func(tokens []string, pos int) *Res {
+		var values []string
+		var children []Res
+		a := 0
+		for a = 0; a < len(tokens) && pos < len(tokens); a++ {
+			res := expr(tokens, pos)
+
+			// Cannot match anymore ?
+			if res == nil {
+				if a < min {
+					return nil
+				}
+				return &Res{
+					Pos: pos,
+					Expr: "greedy",
+					Children: children,
+					Value: strings.Join(values, " "),
+				}
+			}
+
+			values = append(values, res.Value)
+			children = append(children, *res)
+			pos++
+		}
+
+		// Match until the end.
+		if a < min {
+			return nil
+		}
+
+		return &Res{
+			Pos: pos,
+			Expr: "greedy",
+			Children: children,
+			Value: strings.Join(values, " "),
+		}
+	}
+}
+
+func Text(str string) Expression {
+	thisTokens := Tokenize(str)
+	return func(tokens []string, pos int) *Res {
+		if pos + len(thisTokens) > len(tokens) {
+			return nil
+		}
+		for a := 0; a < len(thisTokens); a++ {
+			if thisTokens[a] != tokens[pos + a] {
+				return nil
+			}
+		}
+		return &Res{
+			Pos: pos + len(thisTokens),
+			Expr: "text",
+			Value: str,
+		}
+	}
+}
+
+func CaseInsensitive(tokensForMatch []string) Expression {
+	return func(tokens []string, pos int) *Res {
+		if pos + len(tokensForMatch) > len(tokens) {
+			return nil
+		}
+		for a := 0; a < len(tokensForMatch); a++ {
+			if !strings.EqualFold(tokensForMatch[a], tokens[pos + a]) {
+				return nil
+			}
+		}
+		return &Res{
+			Pos: pos + len(tokensForMatch),
+			Expr: "case_insensitive",
+			Value: strings.Join(tokensForMatch, ""),
+		}
+	}
+}
+
+func Or(expressions ...Expression) Expression {
+	return func(tokens []string, pos int) *Res {
+		if pos >= len(tokens) {
+			return nil
+		}
+		for _, exp := range expressions {
+			res := exp(tokens, pos)
+			if res != nil {
+				return &Res{
+					Pos: res.Pos,
+					Expr: "or",
+					Children: []Res{*res},
+					Value: res.Value,
+				}
+			}
+		}
+		return nil
+	}
+}
+
+func TextUntilEndAt(matchingForEnd Expression) Expression {
+	return func(tokens []string, pos int) *Res {
+		var result []string
+		a := 0
+		for a = 0; a + pos < len(tokens); a++ {
+			if matchingForEnd(tokens, pos + a) != nil {
+				break
+			}
+			result = append(result, tokens[pos + a])
+		}
+		if len(result) == 0 {
+			return nil
+		}
+		return &Res{
+			Pos: pos + a,
+			Expr: "text_until_end_at",
+			Value: strings.Join(result, " "),
+		}
+	}
+}
+
+func TextUntilLineEnd(tokens []string, pos int) *Res {
+	tmp := TextUntilEndAt(Text("\n"))
+	res := tmp(tokens, pos)
+	if res == nil {
+		return nil
+	}
+	res.Expr = "text_until_line_end"
+	return res
+}
+
+func OneTokenExceptLineBreak(tokens []string, pos int) *Res {
+	if tokens[pos] == "\n" {
+		return nil
+	}
+	return &Res{
+		Pos: pos + 1,
+		Expr: "one_token_except_line_break",
+		Value: tokens[pos],
+	}
+}
+
+func PairSeparator(tokens []string, pos int) *Res {
+	main := Or(
+		Greedy(Or(Text(" "), Text("\t"), Text(":")), 2),
+		Text(":"),
+	)
+	res := main(tokens, pos)
+	if res == nil {
+		return nil
+	}
+	return &Res{
+		Pos: res.Pos,
+		Expr: "pair_separator",
+		Value: res.Value,
+	}
+}
+
+func Token(tokens []string, pos int) *Res {
+	return &Res{
+		Pos: pos + 1,
+		Expr: "token",
+		Value: tokens[pos],
+	}
+}
+
+func Email(tokens []string, pos int) *Res {
+	formula := And(Token, Text("@"), Token, Text("."), Token)
+	return formula(tokens, pos)
+}
+
+func Keyword(tokens []string, pos int) *Res {
+	res := tries(tokens, pos)
+	if res == nil {
+		return nil
+	}
+	keyword := res.Value
+	candidateField := mapping[keyword]
+	return &Res{
+		Pos: res.Pos,
+		Expr: "keyword",
+		Value: candidateField,
+	}
+}
+
+func OnePair(tokens []string, pos int) *Res {
+	main := And(Keyword, PairSeparator, TextUntilLineEnd)
+	return main(tokens, pos)
+}
+
+func FullSearchAllKeywords(tokens []string, pos int) *Res {
+
+	// Searching for all keyword-value pairs.
+	expression := FullSearch(OnePair, -1)
+	res := expression(tokens, pos)
+	if res == nil {
+		return nil
+	}
+	detected := map[string]string{}
+	if res.Children != nil {
+		for _, item := range res.Children {
+			keyword := item.Children[0].Value
+			if isset(detected, keyword) {
+				continue
+			}
+			detected[keyword] = item.Children[2].Value
+		}
+	}
+
+	// Searching for email.
+	if _, ok := detected["email"]; !ok {
+		formula := FullSearch(Email, 1)
+		res := formula(tokens, pos)
+		if res != nil {
+			detected["email"] = res.Value
+		}
+	}
+	return &Res{
+		Pos:  res.Pos,
+		Expr: "full_search_all_keywords",
+		Data: detected,
+	}
+}
+
+func MatchLinkedIn(tokens []string, pos int) *Res {
+
+	// Check if it's LinkedIn template.
+	footer := And(
+		Text("Contact"),
+		OneTokenExceptLineBreak,
+		OneTokenExceptLineBreak,
+		OneTokenExceptLineBreak,
+		Text("on"),
+		OneTokenExceptLineBreak,
+		Text("LinkedIn"),
+		)
+	lookupFooter := LookupForOne(footer)
+	res := lookupFooter(tokens, pos)
+	if res == nil {
+		return nil
+	}
+
+	// First line.
+	header := And(
+		Text("Page"),
+		OneTokenExceptLineBreak,
+		Text("1"),
+		OneTokenExceptLineBreak,
+		TextUntilLineEnd,
+		)
+	res = header(tokens, 0)
+	pos = res.Pos
+	fullname := res.Children[4].Value
+
+	// Second line.
+	secondLine := And(
+		TextUntilEndAt(Text("at")),
+		OneTokenExceptLineBreak,
+		TextUntilLineEnd,
+		)
+	res = secondLine(tokens, pos)
+	pos = res.Pos
+	position := res.Children[0].Value
+	workplace := res.Children[2].Value
+
+
+
+	value := Res{
+		Pos: pos,
+		Expr: "linkedin_matching",
+		Data: map[string]string{
+			"fullname": fullname,
+			"position": position,
+			"workplace": workplace,
+		},
+	}
+
+
+	// Third line
+	res = Email(tokens, pos + 1)
+	if res != nil {
+		value.Data["email"] = res.Value
+	}
+
+	return &value
+}
+
+func Main(tokens []string) map[string]string {
+	main := Or(MatchLinkedIn, FullSearchAllKeywords)
+	child := main(tokens, 0)
+	if child == nil {
+		return nil
+	}
+	return Normalize(child.Children[0].Data)
+}
+
+func Normalize(res map[string]string) map[string]string {
+
+    if _, ok := res["email"]; ok {
+        res["email"] = strings.ReplaceAll(res["email"], " ","")
+		res["email"] = strings.ReplaceAll(res["email"], "\t","")
+
+    }
+    if _, ok := res["address"]; ok {
+        res["address"] = strings.ReplaceAll(res["address"], " , ",", ")
+        res["address"] = strings.ReplaceAll(res["address"], " . ",". ")
+        res["address"] = strings.ReplaceAll(res["address"], " / ","/")
+        res["address"] = strings.Trim(res["address"], ", ")
+    }
+    if _, ok := res["birthday"]; ok {
+		res["birthday"] = strings.ReplaceAll(res["birthday"], "\t","")
+		res["birthday"] = strings.ReplaceAll(res["birthday"], " / ","/")
+		res["birthday"] = strings.ReplaceAll(res["birthday"], "/ ","/")
+		res["birthday"] = strings.ReplaceAll(res["birthday"], "/ ","/")
+		res["birthday"] = strings.ReplaceAll(res["birthday"], " , ",", ")
+    }
+    if _, ok := res["phone"]; ok {
+        res["phone"] = strings.ReplaceAll(res["phone"], "( ","(")
+        res["phone"] = strings.ReplaceAll(res["phone"], " )",")")
+    }
+
+    for key, value := range res {
+        value = strings.ReplaceAll(value, "\t", " ")
+        value = strings.ReplaceAll(value, "\r", " ")
+        value = strings.ReplaceAll(value, "\n", " ")
+        value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+		value = strings.ReplaceAll(value, "  ", " ")
+        value = strings.Trim(value, " \t\n\r")
+        res[key] = value
+    }
+	return res
+}
+
+
+
+
+
+
